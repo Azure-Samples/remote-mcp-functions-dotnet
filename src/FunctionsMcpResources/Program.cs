876b24e72dbb39ab75b881d646cdea2a160a9e7f
liliankasem <liliankasem@users.noreply.github.com>
2026-05-11 15:39:24 -0700

    Revert Program.cs to main

--- src/FunctionsMcpResources/Program.cs
+++ src/FunctionsMcpResources/Program.cs
@@ -3,17 +3,14 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using static FunctionsMcpResources.ResourcesInformation;
-using Azure.Monitor.OpenTelemetry.Exporter;
-using Microsoft.Azure.Functions.Worker.OpenTelemetry;
-using OpenTelemetry;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// builder.Services.AddOpenTelemetry()
-//     .UseFunctionsWorkerDefaults()
-//     .UseAzureMonitorExporter();
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
 
 // Configure metadata on resources:
 builder
